1c3788b5be730c2b42fc23d8dca7d93d907fcbcd
JoaogustavoKoche <joaogustavokoche@gmail.com>
2024-08-05 09:53:35 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Janaina  from './janaina';
+
+function App() {
+  return (
+    <div className="App">
+      <Janaina/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/janaina.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import './janaina.css';
+
+export const Janaina = () => {
+  return (
+    <div className='containerDiv'>
+      <h1 className='header'>Janaina Pereira</h1>
+      <div className='text'>
+        <p> 
+          Escrevo isso para voce como forma de falar oque sinto, sinto muito por tudo que aconteceu durante nosso relacionamento, 
+          sei que tivemos momentos ruins mais tambem tivemos varios momentos felizes, 
+          lembro muito bem de quando fui posar ai e ficamos assistindo supernatural e comendo junto com a mel e voce fez panquecas para mim, 
+          foi a primeira vez que comi e foi tao bom, lembro das noites que fizemos X, das noites que fizemos pizzas e muitas outras coisas que fizemos juntos, 
+          como quando fomos ver homem aranha no cinema e principalmente das tardes em que passavamos atras no cemma.
+        </p>
+        <p> 
+          Olhando para traz eu vejo o quao irresponsavel eu fui, e o quao distante eu fui e me arrependo amargamente de todas as vezes em que nao lhe tratei como deveria, 
+          me arrependo de nao ter saido mais com voce e aproveitado sua companhia, aproveitado seu sorriso, sua gargalhada contagiante, 
+          suas historias que voce me contava como as do bode jack voce me ensinou muitas coisas e gracas a voce descobri muitas cosias das quais gostava como resident evil, 
+          comidas, quireras, animes e muitas outras. 
+        </p>
+        <p>
+          Foi com você que aprendi o que era amor e também o que a dor, a dor de perder alguém que você ama ,
+          se hoje eu pudesse voltar no tempo e reviver tudo o que passamos mesmo sabendo que iria acabar eu o faria sem pensar duas vezes, 
+          por mais que doa ,o seu amor valeu cada segundo. 
+        </p>
+        <p>
+         Eu sei que faltou muito da minha parte como namorado, 
+         voce e uma mulher que merece ganhar flores todos os dias nao uma unica vez, 
+         merece alguem que deve estar com voce para tudo que vier e merece alguem que possa lhe fazer bem. 
+        </p>
+        <p>
+          Eu sei que nao devo falar disso 
+        </p>
+        <p>
+          Caso haja uma mínima chance de nós nos reconciliarmos eu quero um dia poder chegar ao estado de como seu pai e sua mae estao juntos, um amando ao outro , 
+          tirando e claro as coisas ruims que ambos passaram no seu relacionando. Eu nao quero ser como os meus. 
+        </p>
+        <p>
+          Voce e a mulher em que quero casar, ter filhos, construir uma familia , e envelhecer junto quero passar cada segundo que me resta com voce e so com voce. 
+        </p>
+      </div>
+      <h2>
+          Joao G. 
+      </h2>
+    </div>
+  );
+}
+
+export default Janaina;
